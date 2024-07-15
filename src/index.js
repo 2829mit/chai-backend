81c35425ce0cc45f97184801e7ab8b6f35512bbb
aditya <1032212829@mitwpu.edu.in>
2024-07-15 23:09:25 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 // import { DB_NAME } from "./constants";
 // import express from "express";
 
+
+// <<<<<<< CODE FOR HOW TO CONNECT DATABASE >>>>>>>>>
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
@@ -11,7 +13,17 @@ dotenv.config({
     path:'./env'
 });
 
-connectDB();
+connectDB()
+
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
 
 
 
---
+++ src/utils/asyncHandlers.js
@@ -0,0 +1,23 @@
+// CODES FOR ERROR HANDLING >>>>>>>>>>>>>>>
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).
+        catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// _____<<<< THIS METHOD IS A WRAPPER FUNCTION THAT WE USE EVERYWHERE .THIS WAS THE 2ND METHOD. FIRST METHOD IS ABOVE THIS ____>>>>>>>>>>
+
+// const asyncHandler = (fn) => async (req,res,next) =>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
