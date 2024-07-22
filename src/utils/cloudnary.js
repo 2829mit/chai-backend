2e156af932198a41203e44e3f7fb12b096e1ea49
aditya <1032212829@mitwpu.edu.in>
2024-07-23 01:22:35 +0530

    how to upload files

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,38 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDNINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDNINARY_API_KEY, 
+        api_secret: process.env.CLOUDNINARY_API_SECRET,  // Click 'View Credentials' below to copy your API secret
+    });
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try {
+        if(!localFilePath) return null
+      const response = await cloudinary.uploader.upload(localFilePath,{ 
+            resource_type:"auto"
+        })
+        console.log("file is uploaded in cloudinary",response.url);
+        return response; 
+
+
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+export  {uploadOnCloudinary}
+
+const uploadResult = await cloudinary.uploader
+.upload(
+    'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+    }
+)
+.catch((error) => {
+    console.log(error);
+});
+
+console.log(uploadResult);
