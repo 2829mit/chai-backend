5bcb11824502fa3aa027bc369d00e51ec2d2d886
aditya <1032212829@mitwpu.edu.in>
2024-08-02 12:09:20 +0530

    fixed token validation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -163,7 +163,7 @@ const logoutUser = asyncHandlers(async (req, res) => {
 const refreshAccessToken = asyncHandlers(async (req, res) => {
     const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
 
-    if (incomingRefreshToken) {
+    if (!incomingRefreshToken) {
         throw new ApiError(401, "unauthorized request")
     }
    try {
